50e5e6e23e3c3a73f9e8c60c68e7d6be267b1153
Keenan <keenanhallas@gmail.com>
2020-09-22 17:22:02 -0500

    testing connection code again

--- data/connection.js
+++ data/connection.js
@@ -1,13 +1,15 @@
-const knex = require('knex');
-const config = require('../knexfile.js');
-const dbEnv = process.env.DB_ENV || 'development';
-module.exports = knex(config[dbEnv]);
+// const knex = require('knex');
+// const config = require('../knexfile.js');
+// const dbEnv = process.env.DB_ENV || 'development';
+// module.exports = knex(config[dbEnv]);
 
-// const knex = require("knex");
-// const knexfile = require("../knexfile");
 
-// const environment = process.env.NODE_ENV || "development";
+//Why doesn't this work?
+const knex = require("knex");
+const knexfile = require("../knexfile");
 
-// const config = knexfile[environment];
+const environment = process.env.DB_ENV || "development";
 
-// module.exports = knex(config);
+const config = knexfile[environment];
+
+module.exports = knex(config);
