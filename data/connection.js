66c071c6154e123d7b1cd8a93d3d563f31e5bc00
Keenan <keenanhallas@gmail.com>
2020-09-22 17:17:32 -0500

    testing connection code

--- data/connection.js
+++ data/connection.js
@@ -1,13 +1,13 @@
-// const knex = require('knex');
-// const config = require('../knexfile.js');
-// const dbEnv = process.env.DB_ENV || 'development';
-// module.exports = knex(config[dbEnv]);
+const knex = require('knex');
+const config = require('../knexfile.js');
+const dbEnv = process.env.DB_ENV || 'development';
+module.exports = knex(config[dbEnv]);
 
-const knex = require("knex");
-const knexfile = require("../knexfile");
+// const knex = require("knex");
+// const knexfile = require("../knexfile");
 
-const environment = process.env.NODE_ENV || "development";
+// const environment = process.env.NODE_ENV || "development";
 
-const config = knexfile[environment];
+// const config = knexfile[environment];
 
-module.exports = knex(config);
+// module.exports = knex(config);
