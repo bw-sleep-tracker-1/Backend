09d728bc61d44ac16bc45b23deb8b1f703460ef3
Keenan <keenanhallas@gmail.com>
2020-09-22 13:27:55 -0500

    un-modified the knexfile production object

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-require('dotenv').config();
+//require('dotenv').config();
 
 module.exports = {
 
@@ -36,35 +36,34 @@ module.exports = {
     }
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
+      tableName: 'knex_migrations'
+    }
   }
-
 };
+
+// production: {
+//   client: 'pg',
+//   connection: process.env.DATABASE_URL,
+//   pool: {
+//     min: 2,
+//     max: 10
+//   },
+//   migrations: {
+//     directory: './data/migrations',
+//   },
+//   seeds: {
+//     directory: './data/seeds',
+//   },
+// }
